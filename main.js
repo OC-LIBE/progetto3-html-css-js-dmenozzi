030311c2e2b5b56900613c292cde9735d6ae07e0
Davide <dmenozzi@bluewin.ch>
2025-06-04 09:36:21 +0200

    js 4 giu

--- main.js
+++ main.js
@@ -6,27 +6,49 @@ async function getPetsData() {
     return petsdata 
 }
 
+function calcAge(date_of_birth){
+    const t = new Date();
+    const current_year = t.getFullYear();
+    const age = current_year - date_of_birth;
 
+    return age
+}
 
 
 async function displayPets(){
 
     const pets = await getPetsData();
     const template = document.querySelector('#animal-card-template')
-    
     const wrapper = document.querySelector('main')
-
     console.log(template)
     
     pets.forEach( pet =>{
         const clone = template.content.cloneNode(true)
 
         // qui modifichiamo il template
-        
-        //aggiorniamo la foto 
+        const name = clone.querySelector(".animal-card-text h1");
+        name.textContent = pet.name;
+
+        const text = clone.querySelector(".animal-card-text p"); 
+        text.textContent = pet.description;
+     
         const image = clone.querySelector('.animal-card-photo img') 
-        image.src=pet.photo
-        //console.log(pet.photo) 
+        image.src = pet.photo;
+        
+        const specie = clone.querySelector(".Specie");
+        specie.textContent = pet.species;
+        
+        const age = clone.querySelector(".Age");
+        const Age = calcAge(pet.birthYear);
+        if (Age < 1){
+            age.textContent = `less than one year old`;
+        }
+        else if (Age == 1){
+            age.textContent = `${Age} year old`;
+        }
+        else {
+            age.textContent = `${Age} years old`;
+        }
 
         //aggiungiamo l'articolo alla pagina
         wrapper.appendChild(clone)
